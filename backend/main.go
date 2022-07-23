0ad89bfedb427af87c313a8b6eaa8eac9ced4273
atsushi-kitazawa <atsushi.kitazawa.1987@gmail.com>
2022-07-24 00:49:05 +0900

    add route get example.

--- backend/main.go
+++ backend/main.go
@@ -1,10 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
+
+type PasswordChanger interface {
+}
+
+type Data struct {
+	Id   int
+	Name string
+}
 
 func main() {
 	doMain()
@@ -12,10 +21,35 @@ func main() {
 
 func doMain() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
+
+	// add route
+	getExam1(r)
+	getExam2(r)
+
 	r.Run(":8080")
+}
+
+func getExam1(r *gin.Engine) {
+	r.GET("/datas", func(c *gin.Context) {
+		datas := make([]Data, 0)
+		datas = append(datas, Data{Id: 1, Name: "foo"})
+		datas = append(datas, Data{Id: 2, Name: "bar"})
+		err := c.BindJSON(datas)
+		if err != nil {
+			c.AbortWithError(http.StatusInternalServerError, err)
+		}
+		c.String(http.StatusOK, fmt.Sprintf("%v", datas))
+	})
+}
+
+func getExam2(r *gin.Engine) {
+	r.GET("/datas/:id", func(c *gin.Context) {
+		_ = c.Param("id")
+		data := Data{Id: 1, Name: "foo"}
+		err := c.BindJSON(data)
+		if err != nil {
+			c.AbortWithError(http.StatusInternalServerError, err)
+		}
+		c.String(http.StatusOK, fmt.Sprintf("%v", data))
+	})
 }
